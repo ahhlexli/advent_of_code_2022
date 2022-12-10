5bdc4ea85d2c0024220a71e25614dde4746c0c80
ahhlexli <alex.li0605@gmail.com>
2022-12-10 16:02:55 +0800

    add: day 6 solutions

---
+++ day06.py
@@ -0,0 +1,36 @@
+def part_one():
+
+    file_name = "day06_input.txt"
+
+    with open(file_name, "r") as f:
+        puzzle_input = f.read()
+
+    for i in range(3, len(puzzle_input)):
+
+        previous_four = puzzle_input[i - 3 : i + 1]
+        previous_four_set = set(previous_four)
+
+        if len(previous_four_set) == 4:
+
+            return i + 1
+
+
+def part_two():
+
+    file_name = "day06_input.txt"
+
+    with open(file_name, "r") as f:
+        puzzle_input = f.read()
+
+    for i in range(13, len(puzzle_input)):
+
+        previous_four = puzzle_input[i - 13 : i + 1]
+        previous_four_set = set(previous_four)
+
+        if len(previous_four_set) == 14:
+
+            return i + 1
+
+
+print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
